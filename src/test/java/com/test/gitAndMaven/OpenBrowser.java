f379ed547dcd65dd1666c3fae70c45202bb186ae
Anjani <you@example.com>
2016-09-29 16:22:20 -0700

    git maven project

---
+++ src/test/java/com/test/gitAndMaven/OpenBrowser.java
@@ -0,0 +1,19 @@
+package com.test.gitAndMaven;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class OpenBrowser {	
+
+	@Test
+	public void OpenBrowserTest()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Software\\chromedriver_win32\\chromedriver.exe");
+		
+		//System.setProperty("webdriver.gecko.driver", "C:\\Selenium\\geckodriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.co.in/");
+	}
+}
