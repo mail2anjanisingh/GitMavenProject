770a9a1bf5e8da277c2501a4bc8dd582106a3b0f
Anjani <you@example.com>
2016-09-29 22:06:04 -0700

    Added New TestCase

--- src/test/java/com/test/gitAndMaven/OpenBrowser.java
+++ src/test/java/com/test/gitAndMaven/OpenBrowser.java
@@ -16,4 +16,13 @@ public class OpenBrowser {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.co.in/");
 	}
+	@Test
+	public void OpenBrowserTest2()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Software\\chromedriver_win32\\chromedriver.exe");
+		
+		//System.setProperty("webdriver.gecko.driver", "C:\\Selenium\\geckodriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.co.in/");
+	}
 }
